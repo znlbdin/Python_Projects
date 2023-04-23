d0b43b4c9ca04c19803dfce820b66f5c60bb2248
Zainal Abidin <53074199+znlbdin@users.noreply.github.com>
2023-04-23 20:29:29 +0700

    Add files via upload

---
+++ projects_d1.py
@@ -0,0 +1,7 @@
+# Project Day 1 | 100 Days of Code with Python
+# Project Name : Band Name Generator
+
+print('Welcome to Band Name Generator 1.0')
+city = input("What's name of the city that you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print('Your band name could be ' + city + ' ' + pet) 
